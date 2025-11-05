eaec36d0ac546be01cf41a00da4c587c7ae13719
JayBradCoding <jbradl24@villanova.edu>
2025-11-05 00:27:51 -0500

    Implemented rate limiting in all api files and created a limiter object in core limiter file

--- app/api/quiz.py
+++ app/api/quiz.py
@@ -1,14 +1,31 @@
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException, Response, Request
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from app.schemas import QuizCreate, QuizUpdate, QuizResponse
 from app.crud import create_quiz, get_quiz, get_quizzes, update_quiz, delete_quiz
-from app.core import get_db
+from app.core import get_db, limiter
 
 router = APIRouter()
 
+@router.get("/quizzes", response_model=list[QuizResponse])
+@limiter.limit("100/minute;1000/hour")
+def get_quizzes_route(request: Request, skip: int, limit: int, db: Session=Depends(get_db)):
+    quizzes = get_quizzes(skip, limit, db)
+    if not quizzes:
+        raise HTTPException(status_code=404, detail="No quizzes found")
+    return quizzes
+
+@router.get("/quizzes/{id}", response_model=QuizResponse)
+@limiter.limit("100/minute;1000/hour")
+def get_quiz_route(request: Request, id: int, db: Session=Depends(get_db)):
+    quiz = get_quiz(id, db)
+    if not quiz:
+        raise HTTPException(status_code=404, detail="No quizzes found")
+    return quiz
+
 @router.post("/quizzes", response_model=QuizResponse)
-def create_quiz_route(data: QuizCreate, db: Session=Depends(get_db)):
+@limiter.limit("10/minute;100/hour")
+def create_quiz_route(request: Request, data: QuizCreate, db: Session=Depends(get_db)):
     try:
         quiz = create_quiz(data, db)
         return quiz
@@ -16,29 +33,17 @@ def create_quiz_route(data: QuizCreate, db: Session=Depends(get_db)):
         db.rollback()
         raise HTTPException(status_code=409, detail="Quiz already exists")
 
-@router.get("/quizzes", response_model=list[QuizResponse])
-def get_quizzes_route(skip: int, limit: int, db: Session=Depends(get_db)):
-    quizzes = get_quizzes(skip, limit, db)
-    if not quizzes:
-        raise HTTPException(status_code=404, detail="No quizzes found")
-    return quizzes
-
-@router.get("/quizzes/{id}", response_model=QuizResponse)
-def get_quiz_route(id: int, db: Session=Depends(get_db)):
-    quiz = get_quiz(id, db)
-    if not quiz:
-        raise HTTPException(status_code=404, detail="No quizzes found")
-    return quiz
-
 @router.patch("/quizzes/{id}", response_model=QuizResponse)
-def update_quiz_route(id: int, data: QuizUpdate ,db: Session=Depends(get_db)):
+@limiter.limit("5/minute;50/hour")
+def update_quiz_route(request: Request, id: int, data: QuizUpdate ,db: Session=Depends(get_db)):
     quiz = update_quiz(id, data.model_dump(exclude_unset=True), db)
     if not quiz:
         raise HTTPException(status_code=404, detail="Quiz not found")
     return quiz
 
 @router.delete("/quizzes/{id}")
-def delete_quiz_route(id: int, db: Session=Depends(get_db)):
+@limiter.limit("5/minute;30/hour")
+def delete_quiz_route(request: Request, id: int, db: Session=Depends(get_db)):
     quiz = get_quiz(id, db)
     if not quiz:
         return Response(status_code=204)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
+from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
+from app.core import limiter
 from app.api import topic_router, lesson_router, quiz_router
 
-limiter = Limiter(key_func=get_remote_address)
 app = FastAPI(
     title="Finlit API",
     version="1.0.0"
